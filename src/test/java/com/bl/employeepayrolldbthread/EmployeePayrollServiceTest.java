021e0ad751253c3db622e78e00636286c2ca87db
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-06 15:46:54 +0530

    [Umesh]Added: test code to add multiple employees to payroll system

--- src/test/java/com/bl/employeepayrolldbthread/EmployeePayrollServiceTest.java
+++ src/test/java/com/bl/employeepayrolldbthread/EmployeePayrollServiceTest.java
@@ -28,29 +28,28 @@ public class EmployeePayrollServiceTest {
 		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
 		Assert.assertEquals(3, entries);
 	}
-	
+
 	@Test
-	public void  givenEmployees_WhenAddedToDB_ShouldMatchEmployeeEntries(){
+	public void givenEmployees_WhenAddedToDB_ShouldMatchEmployeeEntries() {
 		String[] deptArray = new String[4];
 		deptArray[0] = "sales";
 		deptArray[1] = "hr";
 		deptArray[2] = "marketing";
 		deptArray[3] = "hr,sales";
 		EmployeePayrollData[] arrayOfEmps = {
-			new EmployeePayrollData(0, "Jeff bezzos", "M", 100000.0, LocalDate.now(),1,deptArray[0]),
-			new EmployeePayrollData(0, "Bill Gates", "M", 200000.0, LocalDate.now(),2,deptArray[1]),
-			new EmployeePayrollData(0, "Mark Zuckerburg", "M", 300000.0, LocalDate.now(),2,deptArray[2]),
-			new EmployeePayrollData(0, "Sunder", "M", 600000.0, LocalDate.now(),1,deptArray[1]),
-			new EmployeePayrollData(0, "Mukesh", "M", 1000000.0, LocalDate.now(),2,deptArray[2]),
-			new EmployeePayrollData(0, "Anil", "M", 200000.0, LocalDate.now(),1,deptArray[1]),
-		};
+				new EmployeePayrollData(0, "Jeff bezzos", "M", 100000.0, LocalDate.now(), 1, deptArray[0]),
+				new EmployeePayrollData(0, "Bill Gates", "M", 200000.0, LocalDate.now(), 2, deptArray[1]),
+				new EmployeePayrollData(0, "Mark Zuckerburg", "M", 300000.0, LocalDate.now(), 2, deptArray[2]),
+				new EmployeePayrollData(0, "Sunder", "M", 600000.0, LocalDate.now(), 1, deptArray[1]),
+				new EmployeePayrollData(0, "Mukesh", "M", 1000000.0, LocalDate.now(), 2, deptArray[2]),
+				new EmployeePayrollData(0, "Anil", "M", 200000.0, LocalDate.now(), 1, deptArray[1]), };
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		try {
 			employeePayrollService.readEmployeePayrollDataDB(IOService.DB_IO);
-			Instant start =Instant.now();
+			Instant start = Instant.now();
 			employeePayrollService.addEmployeesToPayrollThread(Arrays.asList(arrayOfEmps));
-			Instant end =Instant.now();
-			System.out.println("Duration without thread: "+Duration.between(start, end));
+			Instant end = Instant.now();
+			System.out.println("Duration without thread: " + Duration.between(start, end));
 			Assert.assertEquals(9, employeePayrollService.countEntries(IOService.DB_IO));
 		} catch (employeePayrollException e) {
 		}
